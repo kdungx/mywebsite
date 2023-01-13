9ca6ff9279e3b75853510ab66baf4f8104c3f357
kdungx <dungkath2209058@fpt.edu.vn>
2023-01-13 23:06:40 +0700

    .

--- bai tappp/local storage applications/js/app.js
+++ bai tappp/local storage applications/js/app.js
@@ -1,64 +1,77 @@
 let form = document.getElementById("form");
-let textInput = document.getElementById("dateInput");
+let textInput = document.getElementById("textInput");
 let dateInput = document.getElementById("dateInput");
 let textarea = document.getElementById("textarea");
 let msg = document.getElementById("msg");
 let tasks = document.getElementById("tasks");
 let add = document.getElementById("add");
 
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     formValidation();
-})
+});
 
 let formValidation = () => {
     if (textInput.value === "") {
         console.log("failure");
         msg.innerHTML = "Task cannot be blank";
-    } else {
+
+    }
+    else {
         console.log("success");
         msg.innerHTML = "";
-        acceptDAta();
+        acceptData();
         add.setAttribute("data-bs-dismiss", "modal");
         add.click();
 
         (() => {
-            add.setAttribute("data-vs-dismiss", "");
+            add.setAttribute("data-bs-dismiss", "");
         })();
     }
+
 };
-let data=[{}];
-let acceptDAta = () => {
+
+let data = [{}];
+let acceptData = () => {
     data.push({
         text: textInput.value,
         date: dateInput.value,
         description: textarea.value,
     });
+
     localStorage.setItem("data", JSON.stringify(data));
     console.log(data);
     createTasks();
 };
+
 let createTasks = () => {
     tasks.innerHTML = "";
-    data.map((x,y) => {
-        return (tasks.innerHTML += `
-        <div id=${y}>
-        <span class="fw-blod>${x.text}</span>
-        <p>${x.description}</p>
-        <span class="options">
-            <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
-            <i onClick= "deleteTask(this);createTasks()" class="fas fa-trash-alt"></i>
+    data.map((x, y) => {
+        return (tasks.innerHTML += `     
+        <div id=${y}> 
+            <span class="fw-bold">${x.text}</span>
+            <br>
+            <span class="small text-secondary">${x.date}</span>
+            <p>${x.description}</p>
+            <span class="options">
+                    <i onclick="editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
+                    <i onclick="deleteTask(this);createTasks()" class="fas fa-trash-alt"></i>
             </span>
-        </div>
+        </div>   
         `);
     });
+
     resetForm();
 };
+
+
 let deleteTask = (e) => {
     e.parentElement.parentElement.remove();
     data.splice(e.parentElement.parentElement.id, 1);
     localStorage.setItem("data", JSON.stringify(data));
     console.log(data);
+
 };
 let editTask = (e) => {
     let selectedTask = e.parentElement.parentElement;
@@ -66,15 +79,19 @@ let editTask = (e) => {
     dateInput.value = selectedTask.children[1].innerHTML;
     textarea.value = selectedTask.children[2].innerHTML;
     deleteTask(e);
+
 };
+
 let resetForm = () => {
+
     textInput.value = "";
     dateInput.value = "";
     textarea.value = "";
 };
 
 (() => {
-    data = JSON.parse(localStorage.getItem("data")) || [] 
+    data = JSON.parse(localStorage.getItem("data")) || []
     console.log(data);
     createTasks();
+
 })();
